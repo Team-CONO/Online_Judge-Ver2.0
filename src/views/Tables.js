5f59a5a33559e2e8294b8e2c1390ca138a79d7a5
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-01-24 23:44:20 +0900

    게시판 DB 연동
    나중에 map 함수를 통해 다수의 DB 값을 불러올 예정

--- src/views/Tables.js
+++ src/views/Tables.js
@@ -15,23 +15,14 @@ class Tables extends Component {
         fire()
         this.state = ({islevel: ''})
     }
-    shouldComponentUpdate()
-    {
-      console.log("되는겨 안되는겨");
-      return true;
-    }
-    async componentDidMount() {
-        await firebase
+    render() {
+        firebase
             .database()
             .ref(this.props.level)
             .once('value')
             .then(snapshot => {
                 this.setState({islevel: snapshot.val()})
             });
-        console.log(this.state.islevel.title);
-        console.log(this.state.islevel.body);
-    }
-    render() {
         return (
             <Card small className="my-4 mx-5">
                 <CardBody className="p-0 pb-3">
@@ -52,30 +43,9 @@ class Tables extends Component {
                         <tbody>
                             <tr>
                                 <td>{this.props.level}</td>
-                                <td></td>
-                                <td></td>
+                                <td>{this.state.islevel.title}</td>
+                                <td>어드민</td>
                             </tr>
-                            {/* <tr>
-                        <td>1</td>
-                        <td>Ali</td>
-                        <td>Kerry</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Clark</td>
-                        <td>Angela</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Jerry</td>
-                        <td>Nathan</td>
-                    </tr>
-                    <tr>
-                        <td>4</td>
-                        <td>Colt</td>
-                        <td>Angela</td>
-                    </tr> */
-                            }
                         </tbody>
                     </table>
                 </CardBody>
