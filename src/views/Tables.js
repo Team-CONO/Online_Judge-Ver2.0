b443832017cdb36b1fccf187c048ca1657bea81e
BSAA0203 <33346331+BSAA0203@users.noreply.github.com>
2020-01-29 18:04:36 +0900

    게시판 클릭 이벤트, map 함수를 이용해 다수의 DB 값 불러오기

--- src/views/Tables.js
+++ src/views/Tables.js
@@ -8,23 +8,36 @@ import {
     CardBody
 } from "shards-react";
 import firebase, {fire} from '../Firebase'
+import {Link} from "react-router-dom";
 
 class Tables extends Component {
     constructor(props) {
         super(props)
         fire()
-        this.state = ({islevel: ''})
+        this.state = ({islevel: []})
     }
-    render() {
+    componentDidMount() {
         firebase
             .database()
             .ref(this.props.level)
             .once('value')
             .then(snapshot => {
-                this.setState({islevel: snapshot.val()})
+                snapshot.forEach(item => {
+                    this.setState({
+                        islevel: this
+                            .state
+                            .islevel
+                            .concat(item.val().title)
+                    })
+                })
             });
+    }
+    goto=()=> {
+        alert(this.props.level)
+    }
+    render() {
         return (
-            <Card small className="my-4 mx-5">
+            <Card className="my-4 mx-5">
                 <CardBody className="p-0 pb-3">
                     <table className="table mb-0">
                         <thead className="bg-light">
@@ -41,11 +54,24 @@ class Tables extends Component {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>{this.props.level}</td>
-                                <td>{this.state.islevel.title}</td>
-                                <td>어드민</td>
-                            </tr>
+                            {
+                                this
+                                    .state
+                                    .islevel
+                                    .map((title, index) => {
+                                        return (
+                                            <tr>
+                                                <td>{index + 1}</td>
+                                                <td
+                                                    onClick={this.goto}
+                                                    style={{
+                                                        cursor: 'pointer'
+                                                    }}>{title}</td>
+                                                <td>어드민</td>
+                                            </tr>
+                                        )
+                                    })
+                            }
                         </tbody>
                     </table>
                 </CardBody>
